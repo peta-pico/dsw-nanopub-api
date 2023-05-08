b93d8fb20e38a0439e6e8e9b6ac04062fb6db53c
Tobias Kuhn <kuhntobias@gmail.com>
2023-05-08 13:06:16 +0200

    Update make_tables.sh

--- scripts/make_tables.sh
+++ scripts/make_tables.sh
@@ -2,5 +2,8 @@
 
 set -e
 
-curl -o tables/resource_ids.csv -H "Accept: text/csv" "https://grlc.petapico.org/api-git/peta-pico/dsw-nanopub-api/get_resource_ids"
-curl -o tables/matrix.csv -H "Accept: text/csv" "https://grlc.petapico.org/api-git/peta-pico/dsw-nanopub-api/make_matrix"
+curl -o tables/resource_ids.pre.csv -H "Accept: text/csv" "https://grlc.petapico.org/api-git/peta-pico/dsw-nanopub-api/get_resource_ids"
+cat tables/resource_ids.pre.csv | uniq -u > tables/resource_ids.csv
+
+curl -o tables/matrix.pre.csv -H "Accept: text/csv" "https://grlc.petapico.org/api-git/peta-pico/dsw-nanopub-api/make_matrix"
+cat tables/matrix.pre.csv | uniq -u > tables/matrix.csv
