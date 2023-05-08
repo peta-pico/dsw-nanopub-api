a4bb43c652d7984ddca3463400fc8fb7c7363f53
Tobias Kuhn <kuhntobias@gmail.com>
2023-05-08 11:23:57 +0200

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 echo "This is a test"
+
+curl -o matrix.csv -H "Accept: text/csv" "https://grlc.petapico.org/api-git/peta-pico/dsw-nanopub-api/make_matrix"
+
+ls -l matrix.csv
