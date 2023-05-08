a76ade2b63e8fb686c6a93d5da041cf190f6de45
Tobias Kuhn <kuhntobias@gmail.com>
2023-05-08 12:39:09 +0200

    Update and rename test.sh to make_tables.sh

--- scripts/make_tables.sh
+++ scripts/make_tables.sh
@@ -2,4 +2,5 @@
 
 echo "This is a test"
 
+curl -o tables/resource_ids.csv -H "Accept: text/csv" "https://grlc.petapico.org/api-git/peta-pico/dsw-nanopub-api/resource_ids"
 curl -o tables/matrix.csv -H "Accept: text/csv" "https://grlc.petapico.org/api-git/peta-pico/dsw-nanopub-api/make_matrix"
