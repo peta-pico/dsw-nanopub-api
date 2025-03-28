a5e5fb7b01700aea160263bc7cd3b0f770021ee9
Tobias Kuhn <kuhntobias@gmail.com>
2025-03-28 14:33:46 +0100

    Improve v3 script to create matrix

--- scripts/make_tables_v3.sh
+++ scripts/make_tables_v3.sh
@@ -5,22 +5,30 @@ set -e
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 cd ..
 
+QUERY_API_URL=https://query.knowledgepixels.com
+
 # Get preferred FER IDs:
 
+QUERY=$QUERY_API_URL/api/RAOmIiB2U3ocW6XNRjsfP9MWxHvhN8GeHKdTJONIrYFLo/get-fer-pref-ids
+echo "Calling $QUERY"
 curl -L \
   -o tables/v3/fer-pref-ids-main.pre.csv \
   -H "Accept: text/csv" \
-  https://query.petapico.org/api/RAOmIiB2U3ocW6XNRjsfP9MWxHvhN8GeHKdTJONIrYFLo/get-fer-pref-ids
+  $QUERY
 
+QUERY=$QUERY_API_URL/api/RAkVaxm_xPxkZsOGEWQ0NhJPr8HouSjT9VRkkVkb_r7Ko/get-fer-pref-ids-extra-available
+echo "Calling $QUERY"
 curl -L \
   -o tables/v3/fer-pref-ids-extra-available.pre.csv \
   -H "Accept: text/csv" \
-  https://query.petapico.org/api/RAkVaxm_xPxkZsOGEWQ0NhJPr8HouSjT9VRkkVkb_r7Ko/get-fer-pref-ids-extra-available
+  $QUERY
 
+QUERY=$QUERY_API_URL/api/RAqSFhdnn0xvryQOpaLsm2e8rwlZYpWYWzx7fnuu6wk18/get-fer-pref-ids-extra-to-be-developed
+echo "Calling $QUERY"
 curl -L \
   -o tables/v3/fer-pref-ids-extra-to-be-developed.pre.csv \
   -H "Accept: text/csv" \
-  https://query.petapico.org/api/RAqSFhdnn0xvryQOpaLsm2e8rwlZYpWYWzx7fnuu6wk18/get-fer-pref-ids-extra-to-be-developed
+  $QUERY
 
 csvstack \
   tables/v3/fer-pref-ids-main.pre.csv \
@@ -30,10 +38,12 @@ csvstack \
 
 # Get used FER IDs and combine with table above:
 
+QUERY=$QUERY_API_URL/api/RAjyN5uK7O91bogeShAv9f1iJrXrvbxFmcbZydeyzWYqQ/get-fer-ids
+echo "Calling $QUERY"
 curl -L \
   -o tables/v3/fer-ids.pre.csv \
   -H "Accept: text/csv" \
-  https://query.petapico.org/api/RAjyN5uK7O91bogeShAv9f1iJrXrvbxFmcbZydeyzWYqQ/get-fer-ids
+  $QUERY
 
 csvjoin -d ',' -q '"' -c 'np_pre_pubkey_hash' \
   tables/v3/fer-ids.pre.csv tables/v3/fer-pref-ids.pre.csv \
@@ -41,10 +51,12 @@ csvjoin -d ',' -q '"' -c 'np_pre_pubkey_hash' \
 
 # Get GFF qualifications and combine with table above:
 
+QUERY=$QUERY_API_URL/api/RA-60tM5ao6hpkklW3vInQdBL77M41z5zud3TUGKrx5Q8/get-gff-qualifications
+echo "Calling $QUERY"
 curl -L \
   -o tables/v3/gff-qualifications.pre.csv \
   -H "Accept: text/csv" \
-  https://query.petapico.org/api/RA-60tM5ao6hpkklW3vInQdBL77M41z5zud3TUGKrx5Q8/get-gff-qualifications
+  $QUERY
 
 csvjoin -d ',' -q '"' --left -c 'res_np' \
   tables/v3/fer-ids-long.pre.csv tables/v3/gff-qualifications.pre.csv \
@@ -65,24 +77,30 @@ csvjoin -d ',' -q '"' --left -c 'resource_id' tables/v3/fer-ids-duplicates.pre.c
 
 # Get FER declarations:
 
+QUERY=$QUERY_API_URL/api/RAYcBdAQWI6gveZKNCMJAbGwRblLja04h2ct78GxAw5w0/get-fip-decl-in-index
+echo "Calling $QUERY"
 curl -L \
   -o tables/v3/fip-decl-in-index.pre.csv \
   -H "Accept: text/csv" \
-  https://query.petapico.org/api/RAL8F7s1oIwAooPdg8jLmtRYz9xIigSbt8pgQd1QiXDh0/get-fip-decl-in-index
+  $QUERY
 
+QUERY=$QUERY_API_URL/api/RAJJY0gSdHbzFc1q2sLIPs4ZKSnSFMq_IxxLjixKn80XU/get-fip-decl-details
+echo "Calling $QUERY"
 curl -L \
   -o tables/v3/fip-decl-details.pre.csv \
   -H "Accept: text/csv" \
-  https://query.petapico.org/api/RAJJY0gSdHbzFc1q2sLIPs4ZKSnSFMq_IxxLjixKn80XU/get-fip-decl-details
+  $QUERY
 
 csvjoin -d ',' -q '"' -c 'decl_np' \
   tables/v3/fip-decl-in-index.pre.csv tables/v3/fip-decl-details.pre.csv \
   > tables/v3/fip-declarations.pre.csv
 
+QUERY=$QUERY_API_URL/api/RAz76URtDXiLs-16LwzK-zNyzbIugXW8OjhUQdh5jPdtw/get-fip-supercommunities
+echo "Calling $QUERY"
 curl -L \
   -o tables/v3/fip-supercommunities.pre.csv \
   -H "Accept: text/csv" \
-  https://query.petapico.org/api/RAz76URtDXiLs-16LwzK-zNyzbIugXW8OjhUQdh5jPdtw/get-fip-supercommunities
+  $QUERY
 
 csvjoin -d ',' -q '"' --left -c 'community' \
   tables/v3/fip-declarations.pre.csv tables/v3/fip-supercommunities.pre.csv \
